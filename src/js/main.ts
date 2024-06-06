10496c9bcbd56979d9ba65ea5216378b33297a92
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-06-06 15:14:15 +0300

    added header

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,45 @@
+/* header */
+
+const burger = document.querySelector('.burger');
+const linkClose = document.querySelectorAll('.link-close');
+const overflow = document.querySelector('.overflow');
+
+burger?.addEventListener('click', function () {
+  document.body.classList.toggle('body_lock');
+  document.body.classList.toggle('active');
+});
+
+overflow?.addEventListener('click', function () {
+  document.body.classList.toggle('body_lock');
+  document.body.classList.toggle('active');
+});
+
+for (let i = 0; i < linkClose.length; ++i) {
+  linkClose[i].addEventListener('click', function () {
+  document.body.classList.remove('body_lock');
+  document.body.classList.remove('active');
+  });
+}
+
+/* search */
+
+const searchBtn = document.querySelector('.search-btn');
+const searchFile = document.querySelector('.search-file');
+const overflowBlur = document.querySelector('.overflow-blur');
+
+searchBtn?.addEventListener('click', function () {
+  searchFile?.classList.add('active');
+  overflowBlur?.classList.add('active');
+  document.body.classList.add('body_lock');
+});
+
+overflowBlur?.addEventListener('click', function () {
+  searchFile?.classList.remove('active');
+  overflowBlur.classList.remove('active');
+  document.body.classList.remove('body_lock');
+});
+
+
 // Scroll anim
 
 document.addEventListener('DOMContentLoaded', () => {
